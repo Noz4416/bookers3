4459828f6d1be0ea0573d91afb68ef62c7aabd4c
EC2 Default User <104708989+Noz4416@users.noreply.github.com>
2022-05-18 14:15:32 +0000

    [Update]4-24

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters,if: :devise_controller?
-  before_action :authenticate_user
 
-  def after_signed_in_path_for(resource)
-    about_path
+  def after_sign_in_path_for(resource)
+    user_path
   end
 
-  def after_signed_out_path_for(resource)
-    about_path
+  def after_sign_out_path_for(resource)
+    root_path
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
+  get '/homes/about' => 'homes#about',as:'about'
+  devise_for :users
 
-  
   resources :books,only:[:new,:create,:show,:index]
   resources :users,only:[:show,:edit,:index]
 
-  devise_for :users
-  root to: "homes#top"
-  get '/homes/about' => 'homes#about',as:'about'
+
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
